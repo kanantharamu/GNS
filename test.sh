486b3b99e860bce0ce549a314c9a94728ee69ba7
Karthik A <akarthik101@gmail.com>
2016-11-20 17:44:38 -0500

    Build with logging to file, not console

--- test.sh
+++ test.sh
@@ -1,7 +1,16 @@
 #!/usr/bin/env bash
 
+timestamp() {
+  date +"%s"
+}
+
+
 ant
-for ((i=1; i<=5000; i++)); do
-	echo "Test number $i"
-	ant test
+for ((i=1; i<=1; i++)); do
+	TIMESTAMP=`timestamp`
+	echo "Test number $i, $TIMESTAMP"
+	ant test > ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_log.log
+	zip -r ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_${TRAVIS_BRANCH}_logs.zip ./${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_log.log ./logs/*
+	scp -i ubuntukeypair.pem ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_${TRAVIS_BRANCH}_logs.zip $AWS_USER@$AWS_HOST:$AWS_PATH
+	rm ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_log.log ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_${TRAVIS_BRANCH}_logs.zip
 done
