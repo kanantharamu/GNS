343a17237f5917b3519c8bd3ad146a9436adc0cd
Karthik A <akarthik101@gmail.com>
2016-11-28 18:18:29 -0500

    Added test_010 with more logging

--- test.sh
+++ test.sh
@@ -11,6 +11,6 @@ for ((i=1; i<=5000; i++)); do
 	echo "Test number $i, $TIMESTAMP"
 	ant test > ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_log.log
 	zip -r ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_${TRAVIS_BRANCH}_logs.zip ./${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_log.log ./logs/*
-	scp -i ubuntukeypair.pem ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_${TRAVIS_BRANCH}_logs.zip $AWS_USER@$AWS_HOST:$AWS_PATH
+	scp -i id_rsa ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_${TRAVIS_BRANCH}_logs.zip $AWS_USER@$AWS_HOST:$AWS_PATH
 	rm ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_log.log ${TRAVIS_BUILD_NUMBER}_${TIMESTAMP}_${TRAVIS_BRANCH}_logs.zip
 done
